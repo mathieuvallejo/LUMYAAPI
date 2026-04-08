5c57df00e683f43c0bac6f0a45374b78af300d84
noabiller66 <billernoa0@gmail.com>
2026-04-08 11:08:46 +0200

    commentaire(controllers,models,toutes)

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 const userRouter = require('./user');
 const sessionRouter = require('./session');
 const videoRouter =require('./video')
+const commentaireRouteur =require('/.commentaire')
 
 const router = Router();
 router.use('/users', userRouter);
 router.use('/session', sessionRouter);
 router.use('/video',videoRouter)
+router.use('/commentaire',commentaireRouteur)
 
 module.exports = router;
