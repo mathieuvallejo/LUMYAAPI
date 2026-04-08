d106432debc14ca5974fb55b04a04ed25bd9cf27
noabiller66 <billernoa0@gmail.com>
2026-04-08 11:42:43 +0200

    correction commentaire

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const userRouter = require('./user');
 const sessionRouter = require('./session');
 const videoRouter =require('./video')
-const commentaireRouteur =require('/.commentaire')
+const commentaireRouteur =require('./commentaire')
 
 const router = Router();
 router.use('/users', userRouter);
