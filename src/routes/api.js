9fe4fa5aa31362c64a9317bef3c2bc7ed7bf5127
Lilian <lilian12345@hotmail.fr>
2026-04-08 10:15:37 +0200

    route pour doc

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,6 +4,10 @@ import sessionRouter from './session.js';
 import videoRouter from './video.js';
 import commentaireRouter from './commentaire.js';
 import likeRouter from './likes.js';
+const { Router } = require('express');
+const userRouter = require('./user');
+const sessionRouter = require('./session');
+const docRouter = require('./doc');
 
 const router = Router();
 
@@ -12,5 +16,6 @@ router.use('/session', sessionRouter);
 router.use('/video', videoRouter);
 router.use('/commentaire', commentaireRouter);
 router.use('/likes', likeRouter);
+router.use('/doc', docRouter);
 
 export default router;
