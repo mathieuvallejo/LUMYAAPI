05f13e7b01a1786c54467371f7b7d0ee415881ba
Lilian <lilian12345@hotmail.fr>
2026-04-08 10:15:37 +0200

    route pour doc

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const userRouter = require('./user');
 const sessionRouter = require('./session');
+const docRouter = require('./doc');
 
 const router = Router();
 router.use('/users', userRouter);
 router.use('/session', sessionRouter);
+router.use('/doc', docRouter);
 
 module.exports = router;
